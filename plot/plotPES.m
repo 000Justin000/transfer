df8030ed371e2e3de4650c1c1b24a1b41c2b7173
Junteng Jia <juntengjia@hotmail.com>
2017-05-10 01:07:39 -0400

    added path

--- plot/plotPES.m
+++ plot/plotPES.m
@@ -1,7 +1,7 @@
-    clc, clear, close all;
+clc, clear, close all;
 n = 36;
 
-A = dlmread('../qchem_scan_1111_B3LYP_d3_6-31G*/energies');
+A = dlmread('../qchem_scan_1101_B3LYP_d3_6-31G*/energies');
 X = reshape(A(:,1), [n,n]);
 Y = reshape(A(:,2), [n,n]);
 V = reshape(A(:,3), [n,n]);
@@ -25,10 +25,10 @@ surf(Xe/(2*pi)*360,Ye/(2*pi)*360,Ve);
 view(0,90);
 set(gca,'xlim',[0 360]);
 set(gca,'ylim',[0 360]);
-set(gca,'XTick',[0:60:360]);
-set(gca,'YTick',[0:60:360]);
-set(gca,'XTickLabel',['  0';' 60';'120';'180';'240';'300';'360']);
-set(gca,'YTickLabel',['  0';' 60';'120';'180';'240';'300';'360']);
+set(gca,'XTick',[0:10:360]);
+set(gca,'YTick',[0:10:360]);
+% set(gca,'XTickLabel',['  0';' 60';'120';'180';'240';'300';'360']);
+% set(gca,'YTickLabel',['  0';' 60';'120';'180';'240';'300';'360']);
 xlabel('dihedral-1')
 ylabel('dihedral-2')
 set(gcf, 'Position', [0, 0, 1100, 1200]);
@@ -36,4 +36,4 @@ h = colorbar;
 ylabel(h,'kcal/mol')
 caxis([0 10]);
 shading interp;
-print('~/Desktop/1111', '-depsc')
+print('~/Desktop/1101', '-deps')
