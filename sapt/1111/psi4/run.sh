5fa69ca6643ec23285caafed7d5ec8d86b66e901
Junteng Jia <juntengjia@hotmail.com>
2018-02-04 18:34:26 -0500

    added validate

--- sapt/1111/psi4/run.sh
+++ sapt/1111/psi4/run.sh
@@ -1,4 +1,10 @@
-export OMP_NUM_THREADS=1
-psi4 -i 00_0.in -o 00_0.out -n 1
-psi4 -i 01_0.in -o 01_0.out -n 1
-psi4 -i 02_0.in -o 02_0.out -n 1
+export OMP_NUM_THREADS=4
+# psi4 -i 00_0.in -o 00_0.out -n 1
+# psi4 -i 01_0.in -o 01_0.out -n 1
+# psi4 -i 02_0.in -o 02_0.out -n 1
+psi4 -i 03_0.in -o 03_0.out -n 4
+psi4 -i 03_1.in -o 03_1.out -n 4
+psi4 -i 03_2.in -o 03_2.out -n 4
+psi4 -i 08\*_0.in -o 08\*_0.out -n 4
+psi4 -i 08\*_1.in -o 08\*_1.out -n 4
+psi4 -i 08\*_2.in -o 08\*_2.out -n 4
